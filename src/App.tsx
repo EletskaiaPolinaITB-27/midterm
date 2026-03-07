caafe57ed0cb1879652b4a78aa1f0a3d5c86557f
Polina Eletskaia <polina.e@compsscollege.art>
2026-03-07 20:45:04 +0600

    final

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes } from "react-router"
 import { Route } from "react-router"
 import { UsersPage } from "./pages/usersPage/UsersPage"
+import { UserDetailsPage } from "./pages/userDetails/userDetailsPage"
 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<UsersPage/>}/>
+        <Route path="/users/:id" element={<UserDetailsPage/>}/>
       </Routes>
     </div>
   )
